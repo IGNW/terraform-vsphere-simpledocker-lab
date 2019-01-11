1362f1d9e56d1a51bc53dcb4ad033596e5b63b2b
Tim Crall <timcrall@gmail.com>
2019-01-11 14:41:17 -0800

    Refactors ssh_user to be defined at top level

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 module "lamp-server" {
   source                  = "github.com/IGNW/terraform-vsphere-lamp"
 
-  hostname                = "lamp"
+  hostname                = "lamp2"
   domain                  = "ignw.io"
   vsphere_server          = "10.254.252.5"
   vsphere_datacenter      = "POC-Lab"
   vsphere_datastore       = "IGNW-POC"
   vsphere_compute_cluster = "POC"
   vsphere_network         = "ignw-poc|vesta-devops|servers"
-  disk_template           = "CentOS7_base_template"
+  disk_template           = "ubuntu1604_base_template"
   num_cpus                = 2
   memory_mb               = 4000
   root_volume_size        = 20
